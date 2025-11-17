ec8118b05b41b63bbccca1a694b51165801b526f
Abhishek Srinivasan <abhisheksrinivasan5@gmail.com>
2025-11-17 17:07:37 +0530

    Remove node_modules and dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { Packlyze } from './analyzer/packlyze.js';
-export * from './types.js';
-export { generateHTMLReport } from './visualization/reports.js';
-//# sourceMappingURL=index.d.ts.map
